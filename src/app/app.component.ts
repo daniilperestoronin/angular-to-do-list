4ffe2ae9e9866ec0f6e1277fc1c1a828f02c5df2
daniilperestoronin <perestoronin.daniil@gmail.com>
2019-06-18 17:32:46 +0300

    Add todo Array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-to-do-list-new';
+  todoList = [
+    {
+      name: 'Task 1',
+      comment: 'comment 1',
+      content: 'content 1'
+    },
+    {
+      name: 'Task 2',
+      comment: 'comment 2',
+      content: 'content 2'
+    },
+    {
+      name: 'Task 3',
+      comment: 'comment 3',
+      content: 'content 3'
+    },
+    {
+      name: 'Task 4',
+      comment: 'comment 4',
+      content: 'content 4'
+    }
+  ];
 }
